56adc7823d1d24812189069485244bee6f1af88c
Mitchell <miy456@mail.usask.ca>
2021-07-12 22:08:33 -0600

    Populated the landing page and added a styles folder

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-
-import medical from '../stock_medical.jpg';
+import hero from '../stock_medical.png';
+import '../styles/LandingPage.css';
 
 export default function LandingPage() {
     return (
         <div className="landing-page-root">
-            <div className="imgBox">
-            <img className="hero-image" src={medical} alt="VR Doctor" width='100%'></img>
+            <img src={hero} className='hero-image' height='100%' width='50%'/>
+            <div className='landing-page-content'>
+                Welcome to &lt;CompanyName&gt; - Technical Review
+                <button>
+                    LOG IN
+                </button>
             </div>
         </div>
     )
