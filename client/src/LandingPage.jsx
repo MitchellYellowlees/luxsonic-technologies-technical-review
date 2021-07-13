ab91f1a6b8a64ac596789e98c0deec0ff4984240
Mitchell <miy456@mail.usask.ca>
2021-07-12 21:01:42 -0600

    Created React frontend boilerplate with a landing page consisting of a hero image

---
+++ client/src/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import medical from './stock_medical.jpg';
+
+export default function LandingPage() {
+    return (
+        <div className="landing-page-root">
+            <div className="imgBox">
+            <img className="hero-image" src={medical} alt="VR Doctor" width='100%'></img>
+            </div>
+        </div>
+    )
+}
