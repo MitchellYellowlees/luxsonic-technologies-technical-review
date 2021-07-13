97b0a4cc39cb82d1a29ea81bbcd5562c8dba892e
Mitchell <miy456@mail.usask.ca>
2021-07-13 15:08:04 -0600

    Created a NavBar and a style sheet for it

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import '../styles/NavBar.css'
+
+export default function NavBar() {
+
+    return (
+        <div className="navbar-root">
+            
+        </div>
+    )
+}
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import hero from '../stock_medical.png';
 import '../styles/LandingPage.css';
+import NavBar from '../components/NavBar.jsx';
 
 export default function LandingPage() {
     return (
         <div className="landing-page-root">
+            <NavBar/>
             <img src={hero} className='hero-image' height='100%' width='50%'/>
             <div className='landing-page-content'>
                 Welcome to &lt;CompanyName&gt; - Technical Review
