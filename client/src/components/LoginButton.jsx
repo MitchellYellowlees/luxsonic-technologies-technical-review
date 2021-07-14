56253ab2d6ec8dad0d99840318303fd32c18e56b
Mitchell <miy456@mail.usask.ca>
2021-07-13 21:10:43 -0600

    Added styling to the login button

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import '../styles/LoginButton.css'
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
-    return <button onClick={() => loginWithRedirect()}>LOG IN</button>;
+    return <button className="login" onClick={() => loginWithRedirect()}>LOG IN</button>;
 };
 
 export default LoginButton;
