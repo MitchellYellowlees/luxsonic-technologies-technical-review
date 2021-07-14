ab79c4a4e97c030f6e891f0403fb54e1ebc856bc
Mitchell <miy456@mail.usask.ca>
2021-07-13 19:40:05 -0600

    Populated the Data Entry Dashboard with visual elements and moved the logout button to the NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import '../styles/NavBar.css'
+import { useAuth0 } from '@auth0/auth0-react'
+import LogoutButton from './LogoutButton'
 
 export default function NavBar() {
 
+    const {isAuthenticated} = useAuth0()
+
     return (
         <div className="navbar-root">
-            
+            {isAuthenticated ? (<><LogoutButton/></>) : (<></>)}
         </div>
     )
 }
