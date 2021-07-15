26a39c353a12813c80ccbcaea8870fed08c8568b
Mitchell <miy456@mail.usask.ca>
2021-07-14 22:07:12 -0600

    Added data entry retrieval and viewing and styled the data viewing dashboard

--- client/src/pages/DataViewingDashboard.jsx
+++ client/src/pages/DataViewingDashboard.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 import '../styles/DataViewingDashboard.css';
+import { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUserInfoAsync, fetchUserEntriesAsync } from '../redux/stores/user/actions';
+import { DataRecord } from '../components/DataRecord';
 
 export default function DataViewingDashboard() {
+
+    const { user } = useAuth0()
+    const dispatch = useDispatch()
+    const userInfo = useSelector((state) => state.user.info)
+    const allEntries = useSelector((state) => state.user.entries)
+
+    useEffect(() => {
+        if (user) {
+            dispatch(fetchUserInfoAsync(user))
+            dispatch(fetchUserEntriesAsync(user.email))
+        }
+    }, [])
+
+
+    const renderEntries = () => {
+        //console.log(allEntries.length)
+        if(allEntries!=null){
+        if (allEntries.length!=0){
+            return allEntries.map((entry, i) => {
+                return <DataRecord entry={entry} key={i}/>
+            })
+        }
+    }
+        return <button/>
+    }
+
     return (
         <div className="dashboard-root">
                 <div className="user-greeting">
@@ -10,7 +41,7 @@ export default function DataViewingDashboard() {
                     <button className="export">EXPORT ALL ENTRIES</button>
                 
                 <div className="data-display"> 
-                
+                {renderEntries()}
                 </div>
         </div>
     )
