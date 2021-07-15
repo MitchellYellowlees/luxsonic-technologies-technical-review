b568ffd791dbd420990b4aeb78d548e320e1c5d0
Mitchell <miy456@mail.usask.ca>
2021-07-15 16:53:58 -0600

    moved index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './App';
+import App from './client/src/App';
 
 ReactDOM.render(
   <React.StrictMode>
