b11ff4e0eaa64665adffdcbcf7f56ddfed415f9a
Mitchell <miy456@mail.usask.ca>
2021-07-15 19:15:40 -0600

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ const { server } = require('sinon')
 app.use('/entries', entryRouter)
 app.use('/users', userRouter)
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port: ${PORT}`)
 })
 
